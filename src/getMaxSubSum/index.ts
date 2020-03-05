51ad93354b0809d999d0715eb48181b77bd46f8f
Ramzan Chasygov <theliketurbo@gmail.com>
2020-03-05 14:53:45 +0300

    test: slow solution for getMaxSubSum

--- src/getMaxSubSum/index.ts
+++ src/getMaxSubSum/index.ts
@@ -1,5 +1,16 @@
 const getMaxSubSum = (arr: number[]): number => {
-  return arr.length
+  let maxSum = arr[0]
+
+  for (let i = 0; i < arr.length; i++) {
+    let subMaxSum = 0
+
+    for (let j = i; j < arr.length; j++) {
+      subMaxSum += arr[j]
+      maxSum = Math.max(maxSum, subMaxSum)
+    }
+  }
+
+  return maxSum
 }
 
 export default getMaxSubSum
