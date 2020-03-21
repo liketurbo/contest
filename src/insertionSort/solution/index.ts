20e0557976ab52ea49f45612cd4002ee66fe889d
Ramzan Chasygov <theliketurbo@gmail.com>
2020-03-21 12:58:42 +0300

    feat: implement insertion sort

---
+++ src/insertionSort/solution/index.ts
@@ -0,0 +1,9 @@
+const insertionSort = (arr: number[]) => {
+  for (let i = 1; i < arr.length; i++)
+    for (let j = i; j > 0; j--)
+      if (arr[j - 1] > arr[j]) [arr[j - 1], arr[j]] = [arr[j], arr[j - 1]]
+
+  return arr
+}
+
+export default insertionSort
