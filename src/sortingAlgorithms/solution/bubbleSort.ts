feca29022adb99ba9fc1a7cf41926bd801086fef
Ramzan Chasygov <theliketurbo@gmail.com>
2020-03-22 01:33:45 +0300

    feat: implement bubble sort

--- src/sortingAlgorithms/solution/bubbleSort.ts
+++ src/sortingAlgorithms/solution/bubbleSort.ts
@@ -1,4 +1,8 @@
 const bubbleSort = (arr: number[]): number[] => {
+  for (let i = arr.length - 1; i > 0; i--)
+    for (let j = 0; j < i; j++)
+      if (arr[j] > arr[j + 1]) [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]
+
   return arr
 }
 
