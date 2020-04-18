db2c04de2abce8a87634767aebfc9617175ed5d5
Ramzan Chasygov <theliketurbo@gmail.com>
2020-04-18 12:27:08 +0300

    style(hashTable/test): use shorter names

--- src/hashTable/test/index.test.ts
+++ src/hashTable/test/index.test.ts
@@ -13,20 +13,20 @@ describe(path.basename(path.join(__dirname, "..")), () => {
 
   test("read/write operation (big values)", () => {
     const hashTable = new HashTable()
-    const array = Array.from(new Array(1000), (_, idx) => idx.toString())
+    const arr = Array.from(new Array(1000), (_, idx) => idx.toString())
 
-    for (const num of array) hashTable.set(num, num)
+    for (const num of arr) hashTable.set(num, num)
 
-    for (const num of array) expect(hashTable.get(num)).not.toBeNull()
+    for (const num of arr) expect(hashTable.get(num)).not.toBeNull()
   })
 
   test("resizing works", () => {
     const hashTable = new HashTable()
-    const array = Array.from(new Array(1000), (_, idx) => idx.toString())
+    const arr = Array.from(new Array(1000), (_, idx) => idx.toString())
 
     const oldSize = hashTable.size
 
-    for (const num of array) hashTable.set(num, num)
+    for (const num of arr) hashTable.set(num, num)
 
     expect(hashTable.size).not.toBe(oldSize)
   })
